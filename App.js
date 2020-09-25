f019442205b244d6ef83fd65d9ccf07688e9d098
Ian Scott <chesnekov@gmail.com>
2020-09-25 16:09:31 -0700

    still here, and still studying React-native. Diving into styles and styleSheet.Create();

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!</Text>
-      <Image source={{
-        width: 200,
-        height: 300,
-        uri: 'https://picsum.photos/200/300'
-      }}/>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <Image source={{
+          width: 200,
+          height: 300,
+          uri: 'https://picsum.photos/200/300'
+        }}/>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
