0e86c25975077c40e862ab826080371034239ff0
Ian Scott <chesnekov@gmail.com>
2020-10-03 15:47:42 -0700

    add calendar component

---
+++ src/components/Calendar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import colors from './../config/colors';
+
+function Calendar (props) {
+
+  return(
+    <View>
+      <Text>Calendar Screen</Text>
+    </View>
+  )
+}
+
+export default Calendar;
