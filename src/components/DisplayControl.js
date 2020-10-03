87d7bc35b58919a734d3487aeda0d962d6af0faf
Ian Scott <chesnekov@gmail.com>
2020-10-03 15:16:00 -0700

    add sub component folders for screens yet to come. Add basic logic for updating displayControl visibleState value to WelcomeScreen

--- src/components/DisplayControl.js
+++ src/components/DisplayControl.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import * as a from './../actions';
+import * as a from './../actions/index';
+import * as c from './../actions/ActionTypes';
 import { withFirestore, isLoaded } from 'react-redux-firebase';
+import WelcomeScreen from './WelcomeScreen';
 
 class DisplayControl extends React.Component {
 
@@ -11,6 +13,17 @@ class DisplayControl extends React.Component {
     
   }
 
+  render() {
+    let visibleState ="";
+
+    if(this.props.displayState.display === c.WELCOME_SCREEN) {
+      visibleState= <WelcomeScreen />
+    } else if ()
+
+    return(
+      {visibleState}
+    )
+  }
 }
 
 
