25681da2951ae140761991185d425213e71a1f7f
Ian Scott <chesnekov@gmail.com>
2020-10-05 13:21:56 -0700

    working on the Welcome Screen which is the login screen. Having locally managed dynamic display for login/sign-up

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { ImageBackground, StyleSheet, View, Text, Image, Button } from 'react-native';
 import { welcomeScreen } from '../actions';
 import backgroundImage from '../assets/background2.jpg';
@@ -8,24 +9,77 @@ import firebase from 'firebase/app';
 
 function WelcomeScreen(props) {
 
-  return(
-    <ImageBackground 
-    source={backgroundImage}
-    style={styles.background} >
-      <View style={styles.logoContainer}>
-        <Image source={require('../assets/placeholderLogo.png')} style={styles.logo}/>
-        <Text>Take Charge of Your Health Journey</Text>
-      </View>
+  const [formType, setFormType ] = useState(null);
 
-      <Button onPress={props.goHome} title="Home Page" color={colors.primary} />
-      <View style={styles.loginButton} >
-        <Text>Button will be here!</Text>
-      </View>
-      <View style={styles.registerButton} >
-        <Text>Button will be here!</Text>
-      </View>
-    </ImageBackground>
-  )
+  function doSignUp(event) {
+    event.preventDefault();
+    const email= event.target.email.value;
+  }
+
+
+
+  // Sign in display
+  if (formType === "Sign In") {
+    return (
+      <ImageBackground 
+      source={backgroundImage}
+      style={styles.background} >
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/placeholderLogo.png')} style={styles.logo}/>
+          <Text>Take Charge of Your Health Journey</Text>
+        </View>
+        <View style={styles.loginButton} >
+          <Text>Button will be here!</Text>
+        </View>
+        <View style={styles.registerButton} >
+          <Text>Button will be here!</Text>
+        </View>
+      </ImageBackground>
+    )
+  }
+
+  // Sign up display
+  else if (formType === "Sign Up") {
+    console.log(formType);
+    return (
+      <ImageBackground 
+      source={backgroundImage}
+      style={styles.background} >
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/placeholderLogo.png')} style={styles.logo}/>
+          <Text>Take Charge of Your Health Journey</Text>
+        </View>
+        <View style={styles.loginButton} >
+          <Text>Test</Text>
+        </View>
+        <View style={styles.registerButton} >
+          <Text>Button will be here!</Text>
+        </View>
+      </ImageBackground>
+    )
+  }
+
+  // Default sign-in display
+  else {
+    return (
+      <ImageBackground 
+      source={backgroundImage}
+      style={styles.background} >
+        <View style={styles.logoContainer}>
+          <Image source={require('../assets/placeholderLogo.png')} style={styles.logo}/>
+          <Text>Take Charge of Your Health Journey</Text>
+        </View>
+
+        {/* <Button onPress={props.goHome} title="Home Page" color={colors.primary} /> */}
+        <View style={styles.loginButton} >
+          <Button onPress={() => setFormType("Sign In")} title="Sign In" color={colors.primary} />
+        </View>
+        <View style={styles.registerButton} >
+          <Button onPress={() => setFormType("Sign Up")} title="Sign Up" color={colors.primary} />
+        </View>
+      </ImageBackground>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
