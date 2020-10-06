2f11f5f9ffeee9fb085c3096e74370c73df67e11
Ian Scott <chesnekov@gmail.com>
2020-10-06 11:20:51 -0700

    removes all reference and content for user reducer which is no longer needed

---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import colors from './../config/colors';
+import { StyleSheet, View, Text, Image, Button, TextInput } from 'react-native';
+
+function Navbar (props) {
+
+}
