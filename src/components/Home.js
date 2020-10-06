ab017cfb4c1357a988d16b4bd2bd93c49c61d0c3
Ian Scott <chesnekov@gmail.com>
2020-10-06 16:44:06 -0700

    add some explainging text to Home.js. Starts integration with firebase in MedicationList.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,11 @@ function Home (props) {
 
   return(
     <View>
-      <Text>Home Screen</Text>
+      <Text>This is the Home Screen!</Text>
+
+      <Text>Use the navigation bar to visit the different sectiosn of the project</Text>
+
+      <Text>Please note that only the medications subsection is currently under construction.All other sections are pending</Text>
     </View>
   )
 }
--- src/components/medication/MedicationList.js
+++ src/components/medication/MedicationList.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 import colors from '../../config/colors';
+import { useSelector } from 'react-redux';
 
 function MedicationList(props) {
+
+  useFirestoreConnect([
+    {collection: 'medications'}
+  ]);
+
+  const medications = useSelector(state => state.firestore.ordered.medications);
 
   return(
     <View>
       <Text>MedicationList Screen!</Text>
+
+      
     </View>
   )
 }
