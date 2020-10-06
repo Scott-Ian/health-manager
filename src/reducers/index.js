f319c1542bcd47b4a7dff3b07c0d26d9b03b4d6a
Ian Scott <chesnekov@gmail.com>
2020-10-06 08:38:03 -0700

    add user/login reducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ const { FirebaseReducer } = require("react-redux-firebase")
 import displayReducer from './display-reducer';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
+import {loginReducer } from './login-reducer';
 
 const rootReducer = combineReducers({
   displayState: displayReducer,
   firestore: firestoreReducer,
+  user: loginReducer,
 })
 
 export default rootReducer;
