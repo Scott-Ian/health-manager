5d31f6fb906ee44c20ef302750763fc792e080ca
Ian Scott <chesnekov@gmail.com>
2020-10-06 09:12:35 -0700

    fixes incorrect path for importing color config in sub screens of DisplayControl

--- src/components/appointment/AppointmentCreate.js
+++ src/components/appointment/AppointmentCreate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
-import colors from './../config/colors';
+import colors from '../../config/colors';
 
 function AppointmentCreate(props) {
 
