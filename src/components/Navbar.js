959594bf3798989d9da4de10b80d4995323d6ac5
Ian Scott <chesnekov@gmail.com>
2020-10-06 11:37:49 -0700

    add basic parts of a new navbar component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,5 +3,40 @@ import colors from './../config/colors';
 import { StyleSheet, View, Text, Image, Button, TextInput } from 'react-native';
 
 function Navbar (props) {
+  const { dispatch } = props;
+  
+  function pressHome() {
+    
+  }
+
+  return(
+    <View style={styles.navbar}>
+        <View style={styles.buttonCover}>
+          <Button onPress={() => setFormType("Sign Up")} title="Sign Up" color={colors.primary} />
+        </View>
+        <View style={styles.buttonCover}>
+          <Button onPress={() => setFormType("Sign Up")} title="Sign Up" color={colors.primary} />
+        </View>
+        <View style={styles.buttonCover}>
+          <Button onPress={() => setFormType("Sign Up")} title="Sign Up" color={colors.primary} />
+        </View>
+    </View>
+  )
 
 }
+
+
+const styles = StyleSheet.create({
+  navbar: {
+    flex: 1,
+    justifyContent: "space-around",
+    height: "10%",
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: colors.navbar,
+  },
+  buttonCover: {
+
+  }
+
+});
