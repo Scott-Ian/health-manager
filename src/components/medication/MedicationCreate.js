402d5f0bd9fd2af0e5cbcb1ee73eeb3a560a1dcd
Ian Scott <chesnekov@gmail.com>
2020-10-07 10:59:47 -0700

    add logic for updating firestore and hooks for accessing user input in MedicationCreate.js

--- src/components/medication/MedicationCreate.js
+++ src/components/medication/MedicationCreate.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { useState } from 'react';
+import { View, Text, KeyboardAvoidingView, StyleSheet } from 'react-native';
 import colors from '../../config/colors';
 import * as a from '../../actions/index';
 import { connect } from 'react-redux';
@@ -7,11 +8,66 @@ import PropTypes from 'prop-types';
 
 function MedicationCreate(props) {
 
+  const [ physician, setPhysician ] = useState(null);
+  const [ datePrescribed, setDate ] = useState(null);
+  const [ dosage, setDosage ] = useState(null);
+  const [ quantity, setQuantity ] = useState(null);
+  const [ name, setName ] = useState(null);
+
+  function addMedicationToFirestore() {
+    firestore.collection('medications').add(
+      {
+        physician,
+        datePrescribed,
+        dosage,
+        quantity,
+        name,
+      }
+    );
+
+    const { dispatch } = props;
+    const action = a.medicationList();
+    dispatch(action);
+  }
+
   return(
-    <View>
-      <Text>MedicationCreate Screen</Text>
-    </View>
+    <KeyboardAvoidingView behavior='padding' style={styles.screen} >
+
+      <View style={styles.form}>
+        <Text style={styles.header}>Add a New Medication!</Text>
+
+        
+      </View>
+
+    </KeyboardAvoidingView>
   )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+
+  },
+  form: {
+
+  },
+  header: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    alignSelf: 'center',
+  },
+})
+
+MedicationCreate.propTypes = {
+  displayState: PropTypes.object,
+}
+
+const mapStateToProps = state => {
+  return{
+    displayState: state.displayState,
+    firestore: state.firestore,
+  }
+}
+
+MedicationCreate= connect(mapStateToProps)(MedicationCreate);
 
 export default MedicationCreate;
