9284adbe91851b89c42e065fb4f2ea1222ee82af
Ian Scott <chesnekov@gmail.com>
2020-10-07 14:54:03 -0700

    update appointment list with read and filter funcitonality to only display the users data

--- src/components/appointment/AppointmentList.js
+++ src/components/appointment/AppointmentList.js
@@ -1,14 +1,93 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Button } from 'react-native';
+import { useFirestoreConnect } from 'react-redux-firebase';
 import colors from '../../config/colors';
+import * as a from '../../actions/index';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withFirestore } from 'react-redux-firebase';
 
 function AppointmentList(props) {
 
+  useFirestoreConnect([
+    {collection: 'appointments'}
+  ]);
+
+  const auth = props.firebase.auth();
+  const currentUserEmail = auth.currentUser.email;
+
+  const appointments = props.firestore.data.appointments
+
+  const filterObjectToArray = function(object, currentUserEmail) {
+    let result = [];
+    
+    for(const key in object) {
+      console.log("Object[Key]")
+      console.log(object[key]);
+      if(object[key].userEmail === currentUserEmail) {
+        result.push(object[key]);
+      }
+    }
+    return result;
+  }
+
+  const filteredAppointments = filterObjectToArray(appointments, currentUserEmail);
+
+  function pressNew() {
+    const { dispatch } = props;
+    const action = a.appointmentCreate();
+    dispatch(action);
+  }
+
   return(
-    <View>
-      <Text>AppointmentList Screen</Text>
-    </View>
+    <ScrollView>
+      <Text>Appointment List!</Text>
+
+      <View style={styles.buttonCover}>
+        <Button onPress={pressNew} title="Add New Appointment" color={colors.secondary} />
+      </View>
+
+      {filteredAppointments.map((appointment, index) => {
+        return(
+          <View style={styles.item} key={index}>
+            <Text style={styles.header}>DateTime: {appointment.dateTime}</Text>
+            <Text>Physician: {appointment.physician}</Text>
+            <Text>Location: {appointment.location}</Text>
+            <Text></Text>
+          </View>
+        )
+      })}
+
+    </ScrollView>
   )
 }
 
-export default AppointmentList;
+const styles = StyleSheet.create({
+  buttonCover: {
+    backgroundColor: colors.primary,
+  },
+  header: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    alignSelf: 'center',
+  },
+  item: {
+    alignContent: 'center',
+    borderBottomWidth: 4,
+  },
+})
+
+AppointmentList.propTypes = {
+  displayState: PropTypes.object,
+}
+
+const mapStateToProps = state => {
+  return{
+    displayState: state.displayState,
+    firestore: state.firestore,
+  }
+}
+
+AppointmentList= connect(mapStateToProps)(AppointmentList);
+
+export default withFirestore(AppointmentList);
